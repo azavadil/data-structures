1a9b750745ec15e95521626b5efd588b478db90c
Hack Reactor Students <anthony.zavadil@gmail.com>
2014-06-04 15:26:37 -0700

    complete the function-shared implementation of stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {};
+   stack.logLength = 0;
+   stack.storage = {};
+
+  _.extend(stack, stackMethods);
+  return stack;
 };
 
-var stackMethods = {};
+
+
+stackMethods = {};
+stackMethods.pop = function() {
+  if( this.logLength > 0 ){
+    this.logLength--;
+    var temp = this.storage[this.logLength];
+    delete this.storage[this.logLength];
+    return temp;
+  }
+}
+
+stackMethods.push = function(value) {
+  this.storage[this.logLength] = value;
+  this.logLength++;
+}
+
+stackMethods.size = function(){
+  return this.logLength;
+}
 
 
