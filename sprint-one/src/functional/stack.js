d185682bde8655b8d8846b37d40a1c501a42b165
Hack Reactor Students <anthony.zavadil@gmail.com>
2014-06-04 14:42:49 -0700

    complete function pattern implementation

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,27 @@
 var makeStack = function(){
   var someInstance = {};
+  var size = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function(){
+    if( size > 0 ){
+      size--;
+      var temp = storage[size];
+    }
+    delete storage[size];
+    return temp;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
