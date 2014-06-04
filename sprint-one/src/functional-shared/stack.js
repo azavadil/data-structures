c8857de1d4a51f1a78908e38e223c56aaf0419d8
Hack Reactor Students <anthony.zavadil@gmail.com>
2014-06-04 15:37:11 -0700

    complete functional-shared queue

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -19,15 +19,15 @@ stackMethods.pop = function() {
     delete this.storage[this.logLength];
     return temp;
   }
-}
+};
 
 stackMethods.push = function(value) {
   this.storage[this.logLength] = value;
   this.logLength++;
-}
+};
 
 stackMethods.size = function(){
   return this.logLength;
-}
+};
 
 
